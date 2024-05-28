4819c4d83fd86ecf230cc16e094719deade5f92a
IvanVoloshanskyi <ivan.voloshanskyi@gmail.com>
2024-05-28 11:48:21 +0300

    upgrade admin

--- auto_park/admin.py
+++ auto_park/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from auto_park.car_model import Car, CarType, CarBrand
-from auto_park.models import AutoPark
+from auto_park.models import AutoPark, AutoParkCar
 from auto_park.rent import Rent
 
 
@@ -18,10 +18,42 @@ class RentAdmin(admin.ModelAdmin):
         'rental_cost'
     )
 
+    search_fields = ('user__username', 'user__email',)
+    list_filter = ('user__username',)
+
+
+class AutoParkCarAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'auto_park',
+        'car',
+    )
+    list_filter = ('auto_park__slug',)
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = (
+        'car_brand',
+        'name',
+        'car_type',
+        'year',
+        'price_per_day',
+        'penalty_amount',
+    )
+    list_filter = ('car_brand',)
+
+
+class HiddenModelAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return False
 
 # Register your models here.
-admin.site.register(AutoPark)
-admin.site.register(Car)
-admin.site.register(CarBrand)
-admin.site.register(CarType)
+admin.site.register(AutoParkCar, AutoParkCarAdmin)
+admin.site.register(Car, CarAdmin)
 admin.site.register(Rent, RentAdmin)
+admin.site.register(CarBrand, HiddenModelAdmin)
+admin.site.register(CarType, HiddenModelAdmin)
+admin.site.register(AutoPark, HiddenModelAdmin)
--- auto_park/models.py
+++ auto_park/models.py
@@ -16,3 +16,5 @@ class AutoParkCar(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     is_rented = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.auto_park.slug} | {self.car}'
